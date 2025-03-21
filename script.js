5a612b0989fe6e4978dc892fe364ef458eef2aa2
Sumonnn <sumonkhan1081@gmail.com>
2025-03-21 10:52:41 +0530

    expending cards project done

---
+++ script.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll('.panel');
+
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeAllClasses();
+        panel.classList.add('active');
+    })
+})
+
+function removeAllClasses() {
+    panels.forEach((panel) => {
+        panel.classList.remove('active');
+    })
+}
